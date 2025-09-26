cd26bf061c33e4efbc2c8fea77795c03da8e5ddb
chandanagamage1984 <chandanagamage.clc@gmail.com>
2025-09-26 06:52:11 +0530

    Initial commit of project files

---
+++ Data/EmpMgtSysDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EmployeesManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesManagementSystem.Data
+{
+    public class EmpMgtSysDbContext: DbContext
+    {
+        public EmpMgtSysDbContext(DbContextOptions<EmpMgtSysDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeesManagementSystem.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public double Salary { get; set; }
+    }
+}
