57a5104fb50a93c93ffe628883a79e8e9766184e
chandanagamage1984 <chandanagamage.clc@gmail.com>
2025-09-28 13:44:41 +0530

    Removed db table creaion script

--- employee_table_script.sql
+++
@@ -1,9 +0,0 @@
-use EmployeesManagementSystem
-
-create table employees(
-id int identity(1,1) primary key,
-name nvarchar(128) not null,
-position nvarchar(64) not null,
-department nvarchar(64) not null,
-salary float not null
-)
